2c56d7e822ca9bfc62d426c48c3ff7b57b035a42
Víctor Cuaya <vcuaya@outlook.com>
2024-09-04 23:16:30 -0600

    feat: add console app to calculate floor area

---
+++ Chapter01/04-FloorArea/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+* Representar la lógica de un programa que permita al usuario
+* introducir valores para el ancho y el largo del piso de un
+* salón en metros. El programa da salida al área del piso en
+* metros cuadrados.
+*
+* Represent the logic of a program that allows the user to
+* enter values for the width and length of the floor of a
+* room in meters. The program outputs the area of the floor
+* in square meters.
+*/
+
+namespace FloorArea;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double width, length, area;
+
+        width = ReadNumber("Enter the width of the floor in meters: ");
+        length = ReadNumber("Enter the length of the floor in meters: ");
+        area = width * length;
+
+        Console.WriteLine($"The area of the floor is {area} square meters.");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static double ReadNumber(string message)
+    {
+        double number;
+
+        Console.Write(message);
+
+        while (!double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Invalid input. Try again.");
+        }
+
+        return number;
+    }
+}
