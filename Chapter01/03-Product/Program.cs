1e2f3c2eeb2ad6f7217df2abe1c4917b46aa09b7
Víctor Cuaya <vcuaya@outlook.com>
2024-09-04 22:44:07 -0600

    feat: add console app for product

---
+++ Chapter01/03-Product/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+* Representar la lógica de un programa que permite al usuario introducir
+* dos valores. El programa da salida al producto de ambos valores.
+*
+* Represent the logic of a program that allows the user to input two values.
+* The program outputs the product of both values.
+*/
+
+namespace Product;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double numberA, numberB, result;
+
+        numberA = ReadNumber("Enter the first number: ");
+        numberB = ReadNumber("Enter the second number: ");
+        result = Product(numberA, numberB);
+
+        Console.WriteLine($"The product of {numberA} and {numberB} is {result}");
+        Console.WriteLine("Press any key to exit...");
+    }
+
+    static double ReadNumber(string message)
+    {
+        double userInput;
+
+        Console.Write(message);
+
+        while (!double.TryParse(Console.ReadLine(), out userInput))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+
+        return userInput;
+    }
+
+    static double Product(double numberA, double numberB)
+    {
+        return numberA * numberB;
+    }
+}
