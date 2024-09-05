d82597f6cd21c11cd68811d74fd6587886dfa815
Víctor Cuaya <vcuaya@outlook.com>
2024-09-04 20:24:39 -0600

    feat: Add DivideByTwo console app

---
+++ Chapter01/01-DivideByTwo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DivideByTwo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // This program divides a number by 2
+
+        double number, result;
+
+        number = ReadNumber("Enter a number: ");
+        result = Divide(number, 2);
+
+        Console.WriteLine($"The result of dividing {number} by 2 is {result}");
+        Console.WriteLine("Press any key to exit...");
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    static double ReadNumber(string message)
+    {
+        double userInput;
+
+        Console.WriteLine(message);
+        
+        while (!double.TryParse(Console.ReadLine(), out userInput))
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+
+        return userInput;
+    }
+
+    static double Divide(double number, double divisor)
+    {
+        double result;
+
+        result = number / divisor;
+
+        return result;
+    }
+}
