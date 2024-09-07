30122df1f82e393418f92d5c45d1b25d80ba12a8
Víctor Cuaya <vcuaya@outlook.com>
2024-09-06 23:58:29 -0600

    feat: add console app to calculate body mass index

---
+++ Chapter02/02-BodyMassIndex/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace BodyMassIndex;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double weight = default;
+        double height = default;
+        double weightKg = default;
+        double heightM = default;
+        double bmi = default;
+
+        weight = ReadDouble("Enter your weight in pounds: ");
+        height = ReadDouble("Enter your height in inches: ");
+
+        weightKg = weight * 0.453592;
+        heightM = height * 0.0254;
+        bmi = CalculateBMI(weightKg, heightM);
+
+        Console.WriteLine($"The BMI value is {bmi:F2}");
+        Console.WriteLine("Program finished. Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    static double ReadDouble(string message)
+    {
+        double result = default;
+        Console.Write(message);
+        while (!double.TryParse(Console.ReadLine(), out result))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+            Console.Write(message);
+        }
+        return result;
+    }
+
+    static double CalculateBMI(double weight, double height)
+    {
+        return weight / Math.Pow(height, 2);
+    }
+}
