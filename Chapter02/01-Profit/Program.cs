b6b486dd5e2813668d64ebebde92a54d35bbae86
Víctor Cuaya <vcuaya@outlook.com>
2024-09-06 22:25:29 -0600

    feat: add console app to calculate profit

---
+++ Chapter02/01-Profit/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Profit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double sellingPrice, purchasePrice, profit;
+
+        sellingPrice = ReadDouble("Enter the selling price: ");
+        purchasePrice = ReadDouble("Enter the purchase price: ");
+        profit = sellingPrice - purchasePrice;
+
+        Console.WriteLine($"The profit is {profit:C}");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static double ReadDouble(string message)
+    {
+        double value;
+        Console.WriteLine(message);
+        while (!double.TryParse(Console.ReadLine(), out value))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+        return value;
+    }
+}
