88f39b8209b6236b2a43f2ab482c862d8074c63d
Víctor Cuaya <vcuaya@outlook.com>
2024-09-06 22:54:04 -0600

    feat: update the profit calculation
    - The program now allows calculating the profit for multiple customers until the user enters a sentinel value of zero.

--- Chapter02/01-Profit/Program.cs
+++ Chapter02/01-Profit/Program.cs
@@ -4,13 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        double sellingPrice, purchasePrice, profit;
+        double sellingPrice = default;
+        double purchasePrice = default;
+        double profit = default;
+        double totalProfit = default;
+        const double CENTINEL_PROFIT = 0.0;
 
         sellingPrice = ReadDouble("Enter the selling price: ");
-        purchasePrice = ReadDouble("Enter the purchase price: ");
-        profit = sellingPrice - purchasePrice;
 
-        Console.WriteLine($"The profit is {profit:C}");
+        while (sellingPrice != CENTINEL_PROFIT)
+        {
+            purchasePrice = ReadDouble("Enter the purchase price: ");
+            profit = sellingPrice - purchasePrice;
+            totalProfit += profit;
+
+            Console.WriteLine($"The profit is {profit:C}");
+            
+            sellingPrice = ReadDouble("Enter the selling price: ");
+        }
+
+        Console.WriteLine($"The total profit is {totalProfit:C}");
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
     }
