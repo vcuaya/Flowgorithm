8d16f23d8b7ae2a6ab6735d4ecb89a6bfe48195f
Víctor Cuaya <vcuaya@outlook.com>
2024-09-21 21:42:35 -0600

    feat: add console app to guess number

---
+++ Chapter03/01-GuessNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GuessNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberToGuess = new Random().Next(1, 101);
+        int numberGuessed = default;
+        string message = "Introduce un número entero entre 1 y 100 que creas correcto: ";
+
+        numberGuessed = ReadInteger(message);
+
+        while (numberGuessed != numberToGuess)
+        {
+            if (numberGuessed < numberToGuess)
+            {
+                Console.WriteLine("El número es demasiado bajo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es demasiado alto.");
+            }
+            numberGuessed = ReadInteger(message);
+        }
+
+        Console.WriteLine("¡Felicidades has adivinado! El número era {0}.", numberToGuess);
+        Console.WriteLine("Programa terminado. Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+
+    static int ReadInteger(string message)
+    {
+        int number = default;
+
+        Console.WriteLine(message);
+
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine($"El valor introducido no es un número entero. {message}");
+        }
+
+        return number;
+    }
+}
