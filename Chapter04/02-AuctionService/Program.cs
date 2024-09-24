825e384d4f1d505db6df5bb5314687d311fb7bdb
Víctor Cuaya <vcuaya@outlook.com>
2024-09-24 16:11:43 -0600

    feat: add console app for auction service program

---
+++ Chapter04/02-AuctionService/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace AuctionService;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idNumber = default;
+        int duration = default;
+        decimal minBid = default;
+        string description = default;
+
+        idNumber = ReadInteger("Enter the auction ID number: ");
+        description = ReadString("Enter the auction description: ");
+        duration = ReadInteger("Enter the auction duration in days: ");
+        minBid = ReadDecimal("Enter the minimum bid: ");
+
+        if (100 < minBid)
+        {
+            ShowAuction(idNumber, description, duration, minBid);
+        }
+        else
+        {
+            Console.WriteLine("No auction to display. Minimum bid must be greater than 100.");
+        }
+
+        Console.WriteLine("Program finished. Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    static int ReadInteger(string message)
+    {
+        int number = default;
+        Console.WriteLine(message);
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine($"Please enter a valid integer. {message}");
+        }
+        return number;
+    }
+
+    static decimal ReadDecimal(string message)
+    {
+        decimal number = default;
+        Console.WriteLine(message);
+        while (!decimal.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine($"Please enter a valid decimal. {message}");
+        }
+        return number;
+    }
+
+    static string ReadString(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine();
+    }
+
+    static void ShowAuction(int id, string description, int duration, decimal minBid)
+    {
+        Console.WriteLine("+--------+---------------------------+----------+-------------+");
+        Console.WriteLine("|   ID   |        Description        | Duration | Minimum Bid |");
+        Console.WriteLine("+--------+---------------------------+----------+-------------+");
+        Console.WriteLine($"| {id,6} | {description,-25} | {duration,8} | {minBid,11:C} |");
+        Console.WriteLine("+--------+---------------------------+----------+-------------+");
+    }
+}
