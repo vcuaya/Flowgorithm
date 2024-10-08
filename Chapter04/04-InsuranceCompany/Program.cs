b47fd1ced42d543cbea84193c970319dd59ae44d
Víctor Cuaya <vcuaya@outlook.com>
2024-10-08 12:53:47 -0600

    feat: update console program to read multiple inputs
    - Add a new feature to read multiple inputs from the console while user don't enter 0.
    - Add a new feature to display policy details if age is greater than 35.

--- Chapter04/04-InsuranceCompany/Program.cs
+++ Chapter04/04-InsuranceCompany/Program.cs
@@ -12,47 +12,56 @@ class Program
         int month;
         int year;
         int accidents;
+        const int SENTINEL = 0;
 
-        policyNumber = ReadInteger("Enter the policy number: ");
-        lastName = ReadString("Enter the last name: ");
-        firstName = ReadString("Enter the first name: ");
-        age = ReadInteger("Enter the age: ");
-        day = ReadInteger("Enter the day: ");
-        month = ReadInteger("Enter the month: ");
-        year = ReadInteger("Enter the year: ");
-        accidents = ReadInteger("Enter the number of accidents: ");
+        policyNumber = ReadInteger($"Enter the policy number or type {SENTINEL} to finish: ");
+        while (policyNumber != SENTINEL)
+        {
+            lastName = ReadString("Enter the last name: ");
+            firstName = ReadString("Enter the first name: ");
+            age = ReadInteger("Enter the age: ");
+            day = ReadInteger("Enter the day: ");
+            month = ReadInteger("Enter the month: ");
+            year = ReadInteger("Enter the year: ");
+            accidents = ReadInteger("Enter the number of accidents: ");
 
-        if (policyNumber < 1000 || policyNumber > 9999)
-        {
-            policyNumber = 0;
-        }
-        if (month < 1 || month > 12)
-        {
-            month = 0;
-        }
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-        {
-            if (day < 1 || day > 31)
+            if (policyNumber < 1000 || policyNumber > 9999)
             {
-                day = 0;
+                policyNumber = 0;
             }
-        }
-        else if (month == 4 || month == 6 || month == 9 || month == 11)
-        {
-            if (day < 1 || day > 30)
+            if (month < 1 || month > 12)
             {
-                day = 0;
+                month = 0;
             }
-        }
-        else if (month == 2)
-        {
-            if (day < 1 || day > 29)
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
             {
-                day = 0;
+                if (day < 1 || day > 31)
+                {
+                    day = 0;
+                }
             }
-        }
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                if (day < 1 || day > 30)
+                {
+                    day = 0;
+                }
+            }
+            else if (month == 2)
+            {
+                if (day < 1 || day > 29)
+                {
+                    day = 0;
+                }
+            }
 
-        DisplayPolicy(policyNumber, lastName, firstName, age, day, month, year, accidents);
+            if (age > 35)
+            {
+                DisplayPolicy(policyNumber, lastName, firstName, age, day, month, year, accidents);
+            }
+
+            policyNumber = ReadInteger($"Enter the policy number or type {SENTINEL} to finish: ");
+        }
 
         WriteLine("Program finished. Press any key to exit.");
         ReadKey();
@@ -83,7 +92,7 @@ class Program
         WriteLine("+---------------+-----------------+----------------+-----+-----+-------+------+-----------+");
         WriteLine("| Policy Number |    Last Name    |   First Name   | Age | Day | Month | Year | Accidents |");
         WriteLine("+---------------+-----------------+----------------+-----+-----+-------+------+-----------+");
-        WriteLine($"| {policyNumber, 13} | {lastName, 15} | {firstName, 14} | {age, 3} | {day, 3} | {month, 5} | {year, 4} | {accidents, 9:N0} |");
+        WriteLine($"| {policyNumber,13} | {lastName,15} | {firstName,14} | {age,3} | {day,3} | {month,5} | {year,4} | {accidents,9:N0} |");
         WriteLine("+---------------+-----------------+----------------+-----+-----+-------+------+-----------+");
         WriteLine();
     }
