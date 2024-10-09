c1ee44a2bf8f62494d7c728a76af628461b62f48
Víctor Cuaya <vcuaya@outlook.com>
2024-10-08 18:03:43 -0600

    feat: feat: implement dog daycare registration and fee calculation

---
+++ Chapter04/05-DogDaycare/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace DogDaycare;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idNumber = default;
+        string dogName = default;
+        int dogAge = default;
+        double dogWeight = default;
+        decimal weeklyFee = default;
+
+        idNumber = ReadPositiveInteger("Enter the ID number of the owner: ");
+        dogName = ReadString("Enter the name of the dog: ");
+        dogAge = ReadPositiveInteger("Enter the age of the dog: ");
+        dogWeight = ReadPositiveDouble("Enter the weight of the dog: ");
+        weeklyFee = CalculateWeeklyFee(dogWeight);
+
+        DisplayInvoice(idNumber, dogName, dogAge, dogWeight, weeklyFee);
+
+        WriteLine("Program finished. Press any key to exit.");
+        ReadKey();
+    }
+
+    static int ReadPositiveInteger(string message)
+    {
+        int number = default;
+
+        Write(message);
+
+        while (true)
+        {
+            if (int.TryParse(ReadLine(), out number) && number > 0)
+            {
+                break;
+            }
+
+            Write($"Invalid input. {message}");
+        }
+
+        return number;
+    }
+
+    static string ReadString(string message)
+    {
+        Write(message);
+        return ReadLine();
+    }
+
+    static double ReadPositiveDouble(string message)
+    {
+        double number = default;
+
+        Write(message);
+
+        while (true)
+        {
+            if (double.TryParse(ReadLine(), out number) && number > 0)
+            {
+                break;
+            }
+
+            Write($"Invalid input. {message}");
+        }
+
+        return number;
+    }
+
+    static decimal CalculateWeeklyFee(double dogWeight)
+    {
+        decimal weeklyFee = default;
+
+        if (dogWeight < 7)
+        {
+            weeklyFee = 55m;
+        }
+        else if (dogWeight >= 7 && dogWeight <= 14)
+        {
+            weeklyFee = 75m;
+        }
+        else if (dogWeight > 14 && dogWeight <= 37)
+        {
+            weeklyFee = 105m;
+        }
+        else
+        {
+            weeklyFee = 125m;
+        }
+
+        return weeklyFee;
+    }
+
+    static void DisplayInvoice(int idNumber, string dogName, int dogAge, double dogWeight, decimal weeklyFee)
+    {
+        WriteLine();
+        WriteLine("+----------+--------------------+---------+------------+");
+        WriteLine("| Owner ID |      Dog name      | Dog age | Dog weight |");
+        WriteLine("+----------+--------------------+---------+------------+");
+        WriteLine($"| {idNumber,8} | {dogName,18} | {dogAge,7} | {dogWeight,10:N} |");
+        WriteLine("+----------+--------------------+---------+------------+");
+        WriteLine("+------------------------------------------------------+");
+        WriteLine($"| Weekly Fee:{weeklyFee,41:C} |");
+        WriteLine("+------------------------------------------------------+");
+        WriteLine();
+    }
+}
