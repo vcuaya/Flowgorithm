ee69f011f758a7a3f604687f406380e57529a5b1
Víctor Cuaya <vcuaya@outlook.com>
2024-10-10 18:42:25 -0600

    feat: implement shared transportation
    - Implemented shared transportation to verify that employees are eligible for shared transportation.

---
+++ Chapter04/07-SharedTransportation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SharedTransportation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = default;
+        string city = default;
+
+        name = ReadString("Enter the name of the employee: ");
+        city = ReadString("Enter the city of residence: ");
+
+        if (city.ToLower().Trim() == "woodstock" || city.ToLower().Trim() == "wonderland lake")
+        {
+            WriteLine($"The employee {name} lives in {city} and IS eligible for shared transportation.");
+        }
+        else
+        {
+            WriteLine($"The employee {name} lives in {city} and IS NOT eligible for shared transportation.");
+        }
+
+        WriteLine("Program finished. Press any key to exit.");
+        ReadKey();
+    }
+
+    static string ReadString(string message)
+    {
+        Write(message);
+        return ReadLine();
+    }
+}
