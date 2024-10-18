fb7ee942bb6a16fc6b883cf6215c2750d75af702
Víctor Cuaya <vcuaya@outlook.com>
2024-10-18 15:10:47 -0600

    feat: add program to print one to twenty
    - add program to print one to twenty numbers using a for loop

---
+++ Chapter05/01-PrintOneToTwenty/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PrintOneToTwenty;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if(i == 1)
+            {
+                Write(i);
+            }
+            else
+            {
+                Write(", " + i);
+            }
+        }
+        
+        WriteLine();
+        WriteLine("Program finished. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
