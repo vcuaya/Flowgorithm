8654f40fd8dce4c9175ee818211a024d9bab98fe
Víctor Cuaya <vcuaya@outlook.com>
2024-10-18 18:27:45 -0600

    feat: add program to show even numbers from 1 to 100

---
+++ Chapter05/03-EvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EvenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 100; i += 2)
+        {
+            if (i == 2)
+            {
+                Write(i);
+            }
+            else
+            {
+                Write(", " + i);
+            }
+        }
+
+        WriteLine();
+        WriteLine("Program finished. Press any key to exit.");
+        ReadKey();
+    }
+}
