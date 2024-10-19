9e8947133a4e50979d03d978e97cb734670c368c
Víctor Cuaya <vcuaya@outlook.com>
2024-10-18 19:45:57 -0600

    feat: add print reverse order feature

---
+++ Chapter05/04-ReverseOrder/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReverseOrder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 25; i >= 0; i--)
+        {
+            if (i == 25)
+            {
+                Write(i);
+            }
+            else
+            {
+                Write(", " + i);
+            }
+        }
+    }
+}
