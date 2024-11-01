7ec721d41662a6ff3acecbdf07983583b2dac832
Víctor Cuaya <vcuaya@outlook.com>
2024-11-01 12:03:15 -0600

    feat: add rating guide program

---
+++ Chapter05/12-RatingGuide/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace RatingGuide;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int stars = default;
+        int totalStars = default;
+        int totalClients = default;
+        double avgStars = default;
+
+        stars = ReadInteguer("Enter the number of stars between 0 and 4: ");
+
+        while (stars >= 0)
+        {
+            if (stars > 4)
+            {
+                WriteLine("The number of stars must be between 0 and 4.");
+            }
+            else
+            {
+                totalStars += stars;
+                totalClients++;
+            }
+
+            stars = ReadInteguer("Enter the number of stars between 0 and 4: ");
+        }
+
+        if (totalClients > 0)
+        {
+            avgStars = (double)totalStars / totalClients;
+            WriteLine($"The average number of stars is {avgStars}.");
+        }
+        else
+        {
+            WriteLine("No clients rated.");
+        }
+
+        WriteLine("Program finished. Press any key to exit...");
+        ReadKey();
+    }
+
+    private static int ReadInteguer(string message)
+    {
+        int number = default;
+
+        Write(message);
+
+        while (!int.TryParse(ReadLine(), out number))
+        {
+            WriteLine($"Invalid input. {message}");
+        }
+
+        return number;
+    }
+}
