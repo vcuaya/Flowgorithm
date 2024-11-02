31e76bf4c33c340fae92230f1e021e63de543f31
Víctor Cuaya <vcuaya@outlook.com>
2024-11-02 11:33:17 -0600

    feat: add reverse array program

---
+++ Chapter06/01-ReverseArray/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace ReverseArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int SIZE = 15;
+        int[] numbers = new int[SIZE];
+
+        for (int index = 0; index < SIZE; index++)
+        {
+            numbers[index] = ReadInteger($"Enter an integer number [{index + 1,2}]: ");
+        }
+
+        Write("Numbers in reverse order: ");
+        for (int index = SIZE - 1; index >= 0; index--)
+        {
+            if (index == 0)
+            {
+                Write($"{numbers[index]}.");
+            }
+            else
+            {
+                Write($"{numbers[index]}, ");
+            }
+        }
+
+        WriteLine();
+        WriteLine("Program finished. Press any key to exit...");
+        ReadKey();
+    }
+
+    private static int ReadInteger(string message)
+    {
+        int number = default;
+        Write(message);
+
+        while (!int.TryParse(ReadLine(), out number))
+        {
+            Write($"Invalid input. {message}");
+        }
+
+        return number;
+    }
+}
